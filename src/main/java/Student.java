ae647b6b59499b1a9f20d0b682e80aa413aacbad
12chenkong <channkong582@gmail.com>
2024-08-04 13:10:27 +0700

    inserted

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Scanner;
-
+//this is student class
 public class Student {
     public static void main (String[]args){
                 Scanner scanner = new Scanner(System.in);
@@ -13,7 +13,7 @@ public class Student {
                     System.out.println("3.INSERT BY ID");
                     System.out.println("4.INSERT BY NAME");
                     System.out.println("5.UPDATE BY ID:");
-                    System.out.println("6.DELETE BY id:");
+                    System.out.println("6.DELETE BY id: ");
                     System.out.println("7.SORT BY ID DESC");
                     System.out.println("8.SORT BY NAME A-Z");
                     System.out.println("Please chose one of them:");
